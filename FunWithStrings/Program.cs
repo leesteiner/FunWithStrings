f1a05841a71d55c7c6e25e56688ffc073d8d5964
Lee Steiner <leealansteiner@gmail.com>
2018-03-23 20:34:27 -0400

    4 Methods

--- FunWithStrings/Program.cs
+++ FunWithStrings/Program.cs
@@ -10,6 +10,84 @@ namespace FunWithStrings
     {
         static void Main(string[] args)
         {
+            BasicStringFunctionality();
+            StringConcatenation();
+            EscapeChars();
+            VerbatimString();
+            StringEquality();
+
+        }
+
+        static void BasicStringFunctionality()
+        {
+            Console.WriteLine("=> Basic String functionality:");
+            string firstName = "Freddy";
+            Console.WriteLine("Value of firstName: {0}", firstName);
+            Console.WriteLine("firstName has {0} characters.", firstName.Length);
+            Console.WriteLine("firstName in uppercase: {0}", firstName.ToUpper());
+            Console.WriteLine("firstName in lowercase: {0}", firstName.ToLower());
+            Console.WriteLine("firstName contains the letter y?: {0}", firstName.Contains("y"));
+            Console.WriteLine("firstName after replace: {0}", firstName.Replace("dy", ""));
+            Console.WriteLine();
+        }
+
+        static void StringConcatenation()
+        {
+            Console.WriteLine("=> String Concatenation");
+            string s1 = "Programming the ";
+            string s2 = "PsychoDrill (PTP)";
+            string s3 = s1 + s2;
+            Console.WriteLine(s3);
+            Console.WriteLine();
+        }
+
+        static void EscapeChars()
+        {
+            Console.WriteLine("=> Escape Characters");
+            string strWithTabs = "Model\tColor\tSpeed\tPet Name\a ";
+            Console.WriteLine(strWithTabs);
+
+            Console.WriteLine("Everyone loves \"Hello World!\"\a ");
+            Console.WriteLine("C:\\MyApp\\bin\\Debug\a ");
+
+            //Adds a total of 4 blank lines (then beep again!).
+            Console.WriteLine("All Finished. \n\n\n\a ");
+            Console.WriteLine();
+        }
+
+        static void VerbatimString()
+        {
+            //The following string is printed verbatim,
+            //thus all escape characters are displayed.
+            Console.WriteLine(@"C:\MyApp\bin\Debug");
+
+            //White space is preserved with verbatim strings.
+            string myLongString = @"This is a very
+                very
+                    very
+                        long string";
+            Console.WriteLine(myLongString);
+            Console.WriteLine(@"Cerebus said ""Darrr! Pret-ty sun-sets""");
+        }
+
+        static void StringEquality()
+        {
+            Console.WriteLine("=> String equality");
+            string s1 = "Hello!";
+            string s2 = "Yo!";
+            Console.WriteLine("s1 = {0}", s1);
+            Console.WriteLine("s2 = {0}", s2);
+            Console.WriteLine();
+
+            //Test these strings for equality.
+
+            Console.WriteLine("s1 == s2: {0}", s1 == s2);
+            Console.WriteLine("s1 == Hello!: {0}", s1 == "Hello!");
+            Console.WriteLine("s1 == HELLO!: {0}", s1 == "HELLO!");
+            Console.WriteLine("s1 == hello!: {0}", s1 == "hello!");
+            Console.WriteLine("s1.Equals(s2): {0}", s1.Equals(s2));
+            Console.WriteLine("Yo.Equals(s2): {0}", "Yo!".Equals(s2));
+            Console.WriteLine();
         }
     }
 }
